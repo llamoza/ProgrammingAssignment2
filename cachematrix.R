964634868abf9ddbc073ad6ab72a320bf5dff2c3
llamoza <68175132+llamoza@users.noreply.github.com>
2020-07-12 17:18:55 -0500

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This R script has two functions that compute and cache the inverse of a 
+## non-singular matrix.
 
-## Write a short comment describing this function
+## This function creates a special "matrix" object that can cache its inverse.
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(A = matrix()) {
+  IA <- NULL
+  set <- function(B) {
+    A <<- B
+    IA <<- NULL
+  }
+  get <- function() A
+  setinv <- function(inv) IA <<- inv
+  getinv <- function() IA
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
+## This function computes the inverse (if it exists) of the special "matrix"
+## returned by makeCacheMatrix above
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(A, ...) {
+  ## Return a matrix that is the inverse of 'x'
+  IA <- A$getinv()
+  if(!is.null(IA)) {
+    message("getting cached data")
+    return(IA)
+  }
+  data <- A$get()
+  # Determines if the matrix is non-singular
+  test <- det(data)
+  if(test != 0){
+    IA <- solve(data, ...)
+    A$setinv(IA)
+    IA
+  }
+  else{
+    message("the matrix is singular")
+    A$setinv("the matrix has not inverse")
+  }
 }
